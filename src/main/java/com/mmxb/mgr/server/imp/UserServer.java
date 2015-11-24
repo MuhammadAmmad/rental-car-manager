7752c7d593567cb88d85ffb0ec764c5eff140168
Tree <xingtingshu1994@aliyun.com>
2015-11-24 20:38:11 +0800

    前端页面修改

--- src/main/java/com/mmxb/mgr/server/imp/UserServer.java
+++ src/main/java/com/mmxb/mgr/server/imp/UserServer.java
@@ -11,8 +11,6 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
-import javax.servlet.ServletException;
-import java.io.IOException;
 import java.util.List;
 
 /**
